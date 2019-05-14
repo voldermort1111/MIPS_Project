4228db85df6babc425f5f0bb8e3c647fc77e8eb3
linh37 <voldermort1111@gmail.com>
2019-05-14 09:52:05 +0700

    .

--- Calculator.asm
+++ Calculator.asm
@@ -1,9 +1,9 @@
- #  @copyright (c) 2016, Hedspi, Hanoi University of Technology
- # @author Than Viet Bach
+ #  @copyright (c) 2019, Hedspi, Hanoi University of Technology
+ # @author Nguyen Minh Hieu
  # @version 1.0
  #
-.data 
-  welc:         .asciiz "\nWelcome to Calculator 1.0!\n" 
+.data
+  welc:         .asciiz "\nWelcome to Calculator 1.0!\n"
   p_int:        .asciiz "\n Hay nhap vao so thu nhat co do dai  4 chu so,\n VD: nhan 0021 de nhap so 21 . "
   p_int1:       .asciiz "\n Hay nhap vao so thu hai co do dai  4 chu so, VD: nhan 0001 de nhap so 1 "
   p_toantu:     .asciiz "\n nhap vao toan tu \n Nhan a de nhap phep cong\n Nhan b de nhap phep tru \n Nhan c de nhap phep nhan\n Nhan d de nhap phep chia \n "
@@ -16,10 +16,10 @@
   sb_cong :     .asciiz "\n Ban da nhap phep cong.\n"
   sb_tru:       .asciiz "\n Ban da nhap phep tru. \n"
   sb_daubang:   .asciiz "\n nhap f de hien thi ket qua"
-  rep:          .asciiz "\nNhap 0 de thoat khoi chuong trinh \n Nhap so khac 0 de tiep tuc chuong trinh \n "  
+  rep:          .asciiz "\nNhap 0 de thoat khoi chuong trinh \n Nhap so khac 0 de tiep tuc chuong trinh \n "
   goodbye:      .asciiz "\n Cam on ban da su dung chuong trinh, Tam biet hen gap lai\n"
   erram:        .asciiz "\nKet qua la so am, khong hien thi duoc\n."
-  
+
 .eqv ZERO                   63  # Gia tri byte hien thi so 0 tren den LED
 .eqv ONE                    6  # Gia tri byte hien thi so 1 tren den LED
 .eqv TWO                    91  # Gia tri byte hien thi so 2 tren den LED
@@ -27,39 +27,39 @@
 .eqv FOUR                   102  # Gia tri byte hien thi so 4 tren den LED
 .eqv FIVE                   109  #Gia tri  byte hien thi so 6 tren den LED
 .eqv SIX                    125  #Gia tri  byte hien thi so 6 tren den LED
-.eqv SEVEN                  7  # Gia tri byte hien thi so 7 tren den LED 
+.eqv SEVEN                  7  # Gia tri byte hien thi so 7 tren den LED
 .eqv EIGHT                  127  # Gia tri byte hien thi so 8 tren den LED
 .eqv NINE                   111  # Gia tri byte hien thi so 9 tren den LED
 
 .eqv IN_ADDRESS_HEXA_KEYBOARD   0xFFFF0012  # chua byte dieu khien dong cua ban phim
-                      
+
 .eqv OUT_ADDRESS_HEXA_KEYBOARD  0xFFFF0014  # chua byte tra ve vi tri cua phim duoc bam
-  
+
 .eqv LEFT_LED            0xFFFF0010  # chua byte dieu khien den led ben phai
 .eqv RIGHT_LED          0xFFFF0011  # chua byte dieu khien den led ben trai
-.text 
-main: 
- 
+.text
+main:
+
 start:
      la $a0,welc
-     li $v0, 4     
+     li $v0, 4
      syscall
      la $a0,p_int
-     li $v0, 4     
+     li $v0, 4
      syscall
-  
-    
-      
+
+
+
     #----------------------------------------------------------------------
     # ######### Kich hoat interupt -----------------
     #----------------------------------------------------------------------
-  
-  li $t1,IN_ADDRESS_HEXA_KEYBOARD   
+
+  li $t1,IN_ADDRESS_HEXA_KEYBOARD
   li $t3,0x80     # Kich hoat interupt tu ban phim hexa
   sb $t3,0($t1)
-  
+
     #----------------------------------------------------------------------
-    # Khai bao bien 
+    # Khai bao bien
     #------------2----------------------------------------------------------
       li $t6,0      # $t6: Bien gia tri so cua den LED trai
       li $t7,0      # $t7: Bien gia tri so cua den LED phai
@@ -80,7 +80,7 @@ Loop: beq $s4,4,nhapso2
       beq $s4,4,nhapso2
       nop
       beq $s4,4,nhapso2
-      b Loop  
+      b Loop
       beq $s4,4,nhapso2    #Wait for interrupt
       nop
       beq $s4,4,nhapso2
@@ -88,22 +88,22 @@ Loop: beq $s4,4,nhapso2
     #----------------------------------------------------------------------
 
 nhapso2:
-  
+
     add $s5,$s1,$0 # luu gia tri so thu nhat vao s5
     add $s1,$0,$0 # reset s1
     addi $s4,$0,0 # reset bien dem s4
-    move $a0,$s5 
+    move $a0,$s5
     li $v0,1
-    syscall 
+    syscall
   #li $t2,RIGHT_LED     # RESET LED
   #addi $s0,$zero,0x0000003F
   #sb $s0,0($t2)
-  #li $t2,LEFT_LED 
+  #li $t2,LEFT_LED
   #addi $s0,$zero,0x0000003F
   #sb $s0,0($t2)
-  
+
     li $t6,0      # reset den
-    li $t7,0 
+    li $t7,0
 
     la $a0,p_int1
     li $v0,4
@@ -126,20 +126,20 @@ Loop1: # VOng lap cho interupt so thu 2
   add $s1,$0,$0  #RESET S1
   addi $s4,$0,0 #RESET BIEN DEM S4
     la $a0,xuongdong
-  li $v0, 4     
+  li $v0, 4
   syscall
-  
+
    move $a0,$s6  # IN SO THU 2
   li $v0,1
-  syscall 
-  
-  li $t6,0  # reset den led    
-  li $t7,0 
+  syscall
+
+  li $t6,0  # reset den led
+  li $t7,0
 
   la $a0,p_toantu
   li $v0,4
   syscall
-  
+
 Loop2:
 
     beq $s4,1,nhaptoantu2
@@ -154,7 +154,7 @@ Loop2:
     nop
     beq $s4,1,nhaptoantu2
     b Loop2
-    
+
 nhaptoantu2:
      add $a3,$0,$0
      add $s1,$0,$0 # reset s1
@@ -162,12 +162,12 @@ nhaptoantu2:
 
    cong1: bne $s2,1,tru1
     	  la $a0,sb_cong
-	  li $v0, 4     
+	  li $v0, 4
   	  syscall
   	  j baonhapdaubang
    tru1:  bne $s2,2,nhan1
    	  la $a0,sb_tru
-          li $v0, 4     
+          li $v0, 4
           syscall
           j baonhapdaubang
    nhan1: bne $s2,3,chia1
@@ -177,15 +177,15 @@ nhaptoantu2:
           j baonhapdaubang
    chia1: bne $s2,4,baonhapdaubang
           la $a0,sb_chia
-          li $v0, 4     
+          li $v0, 4
           syscall
           j baonhapdaubang
  baonhapdaubang:
  		la $a0,sb_daubang
-  	        li $v0, 4     
+  	        li $v0, 4
                 syscall
- 
- Loop3: # Cho nhap vao dau = de hien thi ket qua  
+
+ Loop3: # Cho nhap vao dau = de hien thi ket qua
    beq $s3,6, show
     nop
     beq $s3,6,show
@@ -198,51 +198,51 @@ nhaptoantu2:
     nop
     beq $s3,6,show
     b Loop3
-  
+
       show:
-   
+
     case_cong:  bne $s2,1,case_tru  # neu la phep cong
                 addu $s7,$s5,$s6 # thuc hien phep cong
-                la $a0,ketqua 
-                li $v0, 4     
+                la $a0,ketqua
+                li $v0, 4
                 syscall
-                move $a0,$s7 # in ket qua ra console 
+                move $a0,$s7 # in ket qua ra console
                 li $v0,1
-                syscall 
+                syscall
                 j showketqua
 
     case_tru:   bne $s2,2,case_nhan
                 la $a0,ketqua
-                li $v0, 4     
+                li $v0, 4
                 syscall
                 sub $s7,$s5,$s6
                 move $a0,$s7
                 li $v0,1
-                syscall 
+                syscall
                 j showketqua
     case_nhan:  bne $s2,3,case_chia
                 la $a0,ketqua
                 li $v0,4
-                syscall 
+                syscall
                 mul $s7,$s5,$s6
                 move $a0,$s7
                 li $v0,1
-                syscall 
+                syscall
                 j showketqua
     case_chia:  bne $s2,4,pheptinhdf
                 la $a0,ketqua
                 li $v0,4
-                syscall 
+                syscall
                 div $s7,$s5,$s6
                 move $a0,$s7
                 li $v0,1
-                syscall 
+                syscall
          	j showketqua
  pheptinhdf: # bao loi
   la $a0, err1
-  li $v0, 4     
+  li $v0, 4
   syscall
-  
+
 showketqua: li $t9,0
             li $t8,0
             slt $k1,$s7,$0 # kiem tra ket  qua la so am hay khong
@@ -253,7 +253,7 @@ showketqua: li $t9,0
 
             div $t8,$t8,10
             mfhi $t8
-napgiatricholed: 
+napgiatricholed:
                  li $t2,LEFT_LED   # hien thi den LED trai
                  add $s0,$zero,$t9 # truyen bien left
                  jal hienthi
@@ -274,14 +274,14 @@ endmain:
        nop
 END:
       la $a0, goodbye
-      li $v0, 4     
+      li $v0, 4
       syscall
 
-      la $v0, 10   
-      syscall    
+      la $v0, 10
+      syscall
      loisoam: # bao loi ket qua am
               la $a0, erram
-              li $v0, 4     
+              li $v0, 4
               syscall
               li $v0, 51
               la $a0,rep
@@ -290,11 +290,11 @@ END:
               nop
               j start
               nop
-              la $v0, 10   
-              syscall    
+              la $v0, 10
+              syscall
 
-       
-   hienthi:    
+
+   hienthi:
 showleds_0: bne $s0,0,showleds_1  # case $s0 = 0
       li $t4,ZERO
       j napgiatri
@@ -328,7 +328,7 @@ showleds_9: bne $s0,9,showleds_df # case $s0 = 9
 showleds_df:  jr $ra
 napgiatri:    sb $t4,0($t2)
 	      jr $ra
-   
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Xu ly khi xay ra interupt
 # Hien thi so vua bam len den led 7 doan
@@ -350,7 +350,7 @@ IntSR:addi $sp,$sp,4    # Save $ra because we may change it later
       sw $t1,0($sp)
       addi $sp,$sp,4    # Save $t3, because we may change it later
       sw $t3,0($sp)
-      addi $sp,$sp,4 
+      addi $sp,$sp,4
       sw $s4,0($sp)
   # ----------------------------------------------------------------------
   # Processing
@@ -371,7 +371,7 @@ next_pc:    mfc0 $at,$14    # $at <= Copro0.$14 = Coproc0.epc
   #-----------------------------------------------------------------------
   # RESTORE the REG FILE from STACK
   #-----------------------------------------------------------------------
-restore:    
+restore:
     #  lw $s4,0($sp)
      # addi $sp,$sp,-4
       lw $t3,0($sp)
@@ -390,11 +390,11 @@ back_main:  eret
 
   #------------------------------------------------------------------------
   # Thu tuc quet cac phim o hang 1 va xu ly
-  # Tham so truyen vao: 
+  # Tham so truyen vao:
   # Tra ve:
   #------------------------------------------------------------------------
 getInt1: addi $sp,$sp,4
-      sw $ra,0($sp) 
+      sw $ra,0($sp)
       li $t1,IN_ADDRESS_HEXA_KEYBOARD
       li $t3,0x81     # Kich hoat interrupt, cho phep bam phim o hang 1
       sb $t3,0($t1)
@@ -441,14 +441,14 @@ getInt1rt: lw $ra,0($sp)
       addi $sp,$sp,-4
       jr $ra
   #-------------------------------------------------------------------------
-    
+
     #------------------------------------------------------------------------
   # Thu tuc quet cac phim o hang 2 va xu ly
-  # Tham so truyen vao: 
+  # Tham so truyen vao:
   # Tra ve:
   #------------------------------------------------------------------------
 getInt2: addi $sp,$sp,4
-      sw $ra,0($sp) 
+      sw $ra,0($sp)
       li $t1,IN_ADDRESS_HEXA_KEYBOARD
       li $t3,0x82     # Kich hoat interrupt, cho phep bam phim o hang 1
       sb $t3,0($t1)
@@ -496,7 +496,7 @@ getInt2rt: lw $ra,0($sp)
       jr $ra
  ##################### GETCODE 3 ##########
  getInt3: addi $sp,$sp,4
-      sw $ra,0($sp) 
+      sw $ra,0($sp)
       li $t1,IN_ADDRESS_HEXA_KEYBOARD
       li $t3,0x84     # Kich hoat interrupt, cho phep bam phim o hang 3
       sb $t3,0($t1)
@@ -520,7 +520,7 @@ case_a:   li $t5,0x44
          bne $t3,$t5,case_b # case 0x44
          addi $s2,$0,1
          j case_default3
-      
+
 case_b:   li $t5,0xffffff84
     bne $t3,$t5,case_default3
     addi $s2,$0,2
@@ -544,7 +544,7 @@ getInt3rt: lw $ra,0($sp)
       jr $ra
 #-----------------------------------------getcode 4
  getInt4: addi $sp,$sp,4
-      sw $ra,0($sp) 
+      sw $ra,0($sp)
       li $t1,IN_ADDRESS_HEXA_KEYBOARD
       li $t3,0x88     # Kich hoat interrupt, cho phep bam phim o hang 4
       sb $t3,0($t1)
@@ -555,7 +555,7 @@ case_c:   li $t5,0x18
           bne $t3,$t5,case_d # case 0x44
           addi $s2,$0,3
           j case_default3
-      
+
 case_d:   li $t5,0x28
           bne $t3,$t5,case_f
           addi $s2,$0,4
@@ -565,11 +565,11 @@ case_d:   li $t5,0x28
 #   bne $t3,$t5,case_f
 #    addi $s2,$0,5
 #    j  case_default4
-case_f:   li $t5,0xffffff88 
+case_f:   li $t5,0xffffff88
           bne $t3,$t5,case_default4
           addi $s3,$0,6
           j case_default4
-  
+
 
 case_default4:   j getInt4rt
 getInt4rt:       lw $ra,0($sp)
@@ -577,7 +577,7 @@ getInt4rt:       lw $ra,0($sp)
                  jr $ra
 
    #-------------------------------------------------------------------------
-    
+
   #-------------------------------------------------------------------------
   # Thu tuc hien thi den LED
   # Tham so truyen vao: $t2 (dia chi cua LEFT_LED hoac RIGHT_LED), $s0 : bien kieu int
@@ -585,7 +585,7 @@ getInt4rt:       lw $ra,0($sp)
   #-------------------------------------------------------------------------
 displayLED: addi $sp,$sp,4
       sw $ra,0($sp)   # save $ra
-display:    
+display:
 hienthi_0: bne $s0,0,hienthi_1  # case $s0 = 0
       li $t4,ZERO
       j assign
